a26b45b77895d3df5ffc446d034ff404f58b2580
Emadeldeen Eldele <37911596+emadeldeen24@users.noreply.github.com>
2020-10-22 14:09:14 +0800

    update

--- batch_train.sh
+++ batch_train.sh
@@ -1,8 +1,11 @@
+gpu=$1
+data=$2
+
 start=0
 end=`cat config.json | jq '.data_loader.args.num_folds'`
 end=$((end-1))
 
 for i in $(eval echo {$start..$end})
 do
-   python train_Kfold_CV.py --fold_id=$i --device 1 --np_data_dir path/to/numpy/files
+   python train_Kfold_CV.py --fold_id=$i --device $gpu --np_data_dir $data
 done
