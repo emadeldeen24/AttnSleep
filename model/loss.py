6aaed68f3fa4043249100d414aad23ae6106a7ee
Emadeldeen Eldele <37911596+emadeldeen24@users.noreply.github.com>
2021-07-22 12:27:01 +0800

    Update loss.py

--- model/loss.py
+++ model/loss.py
@@ -2,6 +2,6 @@ import torch
 import torch.nn as nn
 
 
-def weighted_CrossEntropyLoss(output, target, classes_weights):
-    cr = nn.CrossEntropyLoss(weight=torch.tensor(classes_weights).cuda())
+def weighted_CrossEntropyLoss(output, target, classes_weights, device):
+    cr = nn.CrossEntropyLoss(weight=torch.tensor(classes_weights).to(device))
     return cr(output, target)
